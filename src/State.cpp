dd8f727f1500e74fe7102463c4bbbf3d01c5b163
ngsylar <ngsylar@gmail.com>
2021-09-24 02:15:36 -0300

    indentation adjustment

--- src/State.cpp
+++ src/State.cpp
@@ -5,9 +5,9 @@
 #include "Vec2.h"
 
 State::State () {
-	GameObject* bg;
+    GameObject* bg;
     Sprite* bgSprite;
-    
+
     quitRequested = false;
     music.Open(MUS_BG);
     music.Play(MUS_REPEAT_ON);
@@ -17,10 +17,10 @@ State::State () {
     bg->AddComponent(bgSprite);
     objectArray.emplace_back(bg);
 
-	bg->box = Rect(
-		SPR_START_X, SPR_START_Y,
-		bgSprite->GetWidth(), bgSprite->GetHeight()
-	);
+    bg->box = Rect(
+        SPR_START_X, SPR_START_Y,
+        bgSprite->GetWidth(), bgSprite->GetHeight()
+    );
 }
 
 State::~State () {
@@ -41,9 +41,9 @@ void State::LoadAssets () {
 }
 
 void State::Update (float dt) {
-	Sprite* sprite;
-	Sound* sound;
-	Face* face;
+    Sprite* sprite;
+    Sound* sound;
+    Face* face;
 
     Input();
     for (unsigned i=0; i < objectArray.size(); i++) {
@@ -52,15 +52,15 @@ void State::Update (float dt) {
 
     for (int i=(int)objectArray.size()-1; i >= 0; i--) {
         if (objectArray[i]->IsDead()) {
-			sprite = (Sprite*)objectArray[i]->GetComponent("Sprite");
-			face = (Face*)objectArray[i]->GetComponent("Face");
-			objectArray[i]->RemoveComponent(sprite);
-			objectArray[i]->RemoveComponent(face);
+            sprite = (Sprite*)objectArray[i]->GetComponent("Sprite");
+            face = (Face*)objectArray[i]->GetComponent("Face");
+            objectArray[i]->RemoveComponent(sprite);
+            objectArray[i]->RemoveComponent(face);
 
-			sound = (Sound*)objectArray[i]->GetComponent("Sound");
-			if (!sound->Playing()) {
-            	objectArray.erase(objectArray.begin()+i);
-			}
+            sound = (Sound*)objectArray[i]->GetComponent("Sound");
+            if (!sound->Playing()) {
+                objectArray.erase(objectArray.begin()+i);
+            }
         }
     }
 }
@@ -92,54 +92,54 @@ void State::AddObject (int pX, int pY) {
 }
 
 void State::Input() {
-	SDL_Event event;
-	int mouseX, mouseY;
+    SDL_Event event;
+    int mouseX, mouseY;
 
-	// Obtenha as coordenadas do mouse
-	SDL_GetMouseState(&mouseX, &mouseY);
+    // Obtenha as coordenadas do mouse
+    SDL_GetMouseState(&mouseX, &mouseY);
 
-	// SDL_PollEvent retorna 1 se encontrar eventos, zero caso contrário
-	while (SDL_PollEvent(&event)) {
+    // SDL_PollEvent retorna 1 se encontrar eventos, zero caso contrário
+    while (SDL_PollEvent(&event)) {
 
-		// Se o evento for quit, setar a flag para terminação
-		if(event.type == SDL_QUIT) {
-			quitRequested = true;
-		}
-		
-		// Se o evento for clique...
-		if(event.type == SDL_MOUSEBUTTONDOWN) {
+        // Se o evento for quit, setar a flag para terminação
+        if(event.type == SDL_QUIT) {
+            quitRequested = true;
+        }
 
-			// Percorrer de trás pra frente pra sempre clicar no objeto mais de cima
-			for(int i = (int)objectArray.size() - 1; i >= 0; --i) {
-				// Obtem o ponteiro e casta pra Face.
-				GameObject* go = (GameObject*) objectArray[i].get();
-				// Nota: Desencapsular o ponteiro é algo que devemos evitar ao máximo.
-				// O propósito do unique_ptr é manter apenas uma cópia daquele ponteiro,
-				// ao usar get(), violamos esse princípio e estamos menos seguros.
-				// Esse código, assim como a classe Face, é provisório. Futuramente, para
-				// chamar funções de GameObjects, use objectArray[i]->função() direto.
+        // Se o evento for clique...
+        if(event.type == SDL_MOUSEBUTTONDOWN) {
 
-				if(go->box.Contains( (float)mouseX, (float)mouseY ) ) {
-					Face* face = (Face*)go->GetComponent( "Face" );
-					if ( nullptr != face ) {
-						// Aplica dano
-						face->Damage(std::rand() % 10 + 10);
-						// Sai do loop (só queremos acertar um)
-						break;
-					}
-				}
-			}
-		}
-		if( event.type == SDL_KEYDOWN ) {
-			// Se a tecla for ESC, setar a flag de quit
-			if( event.key.keysym.sym == SDLK_ESCAPE ) {
-				quitRequested = true;
-			}
-			// Se não, crie um objeto
-			else {
-				Vec2 objPos = Vec2( 200, 0 ).GetRotated( -PI + PI*(rand() % 1001)/500.0 ) + Vec2( (float)mouseX, (float)mouseY );
-				AddObject((int)objPos.x, (int)objPos.y);
-			}
-		}
-	}
+            // Percorrer de trás pra frente pra sempre clicar no objeto mais de cima
+            for(int i = (int)objectArray.size() - 1; i >= 0; --i) {
+                // Obtem o ponteiro e casta pra Face.
+                GameObject* go = (GameObject*) objectArray[i].get();
+                // Nota: Desencapsular o ponteiro é algo que devemos evitar ao máximo.
+                // O propósito do unique_ptr é manter apenas uma cópia daquele ponteiro,
+                // ao usar get(), violamos esse princípio e estamos menos seguros.
+                // Esse código, assim como a classe Face, é provisório. Futuramente, para
+                // chamar funções de GameObjects, use objectArray[i]->função() direto.
+
+                if(go->box.Contains( (float)mouseX, (float)mouseY ) ) {
+                    Face* face = (Face*)go->GetComponent( "Face" );
+                    if ( nullptr != face ) {
+                        // Aplica dano
+                        face->Damage(std::rand() % 10 + 10);
+                        // Sai do loop (só queremos acertar um)
+                        break;
+                    }
+                }
+            }
+        }
+        if( event.type == SDL_KEYDOWN ) {
+            // Se a tecla for ESC, setar a flag de quit
+            if( event.key.keysym.sym == SDLK_ESCAPE ) {
+                quitRequested = true;
+            }
+            // Se não, crie um objeto
+            else {
+                Vec2 objPos = Vec2( 200, 0 ).GetRotated( -PI + PI*(rand() % 1001)/500.0 ) + Vec2( (float)mouseX, (float)mouseY );
+                AddObject((int)objPos.x, (int)objPos.y);
+            }
+        }
+    }
 }
