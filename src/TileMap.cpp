7dac55e332fb1643615ee532ddf21db43d397ba4
ngsylar <ngsylar@gmail.com>
2021-11-04 21:12:07 -0300

    parallax

--- src/TileMap.cpp
+++ src/TileMap.cpp
@@ -59,8 +59,8 @@ void TileMap::RenderLayer (int layer, int cameraX, int cameraY) {
             tile = At(x, y, layer);
             tileSet->RenderTile(
                 (unsigned)tile,
-                (float)x*tileSet->GetTileWidth()-cameraX,
-                (float)y*tileSet->GetTileHeight()-cameraY
+                (float)x*tileSet->GetTileWidth()-(0.2*layer+1)*cameraX,
+                (float)y*tileSet->GetTileHeight()-(0.2*layer+1)*cameraY
             );
         }
     }
